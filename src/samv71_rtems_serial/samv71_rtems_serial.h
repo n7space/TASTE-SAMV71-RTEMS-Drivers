d497f2b728939c77d564bc03106413c1915949b8
Rafał Babski <rbabski@n7space.com>
2025-10-14 10:10:02 +0200

    Uart code inside driver

--- src/samv71_rtems_serial/samv71_rtems_serial.h
+++ src/samv71_rtems_serial/samv71_rtems_serial.h
@@ -1,14 +1,39 @@
+/**@file
+ * This file is part of the TASTE SAMV71 RTEMS Drivers.
+ *
+ * @copyright 2025 N7 Space Sp. z o.o.
+ *
+ * Licensed under the ESA Public License (ESA-PL) Permissive (Type 3),
+ * Version 2.4 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://essr.esa.int/license/list
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef SAMV71_RTEMS_SERIAL_H
 #define SAMV71_RTEMS_SERIAL_H
 
+/**
+ * @file     samv71_rtems_serial.h
+ * @brief    Driver for TASTE for SAMV71 UART
+ */
+
 #include "samv71_rtems_serial_internal.h"
+
+#include <rtems.h>
 
 #include <stddef.h>
 #include <stdint.h>
 
 #include <Broker.h>
 #include <Escaper.h>
-#include <rtems.h>
 
 #include <Hal.h>
 
@@ -28,6 +53,13 @@
 					 Serial_SAMV71_RTEMS_UART_TLS_SIZE, \
 				 RTEMS_FLOATING_POINT))
 
+/**
+ * @brief Structure for samv71_rtems_serial driver internal data
+ *
+ * This structure is allocated by runtime and the pointer is passed to all
+ * driver functions. The name of this structure shall match driver definition
+ * from ocarina_components.aadl and has suffix '_private_data'.
+ */
 typedef struct {
 	Serial_SamV71_Rtems_Device_T m_device;
 	Hal_Uart m_hal_uart;
@@ -50,12 +82,44 @@ typedef struct {
 	rtems_id m_tx_semaphore;
 } samv71_rtems_serial_private_data;
 
+/**
+ * @brief Initialize samv71_rtems_serial driver.
+ *
+ * Function is used by runtime to initialize the driver.
+ *
+ * @param private_data                  Driver private data, allocated by
+ * runtime
+ * @param bus_id                        Identifier of the bus, which is driver
+ * @param device_id                     Identifier of the device
+ * @param device_configuration          Configuration of device
+ * @param remote_device_configuration   Configuration of remote device
+ */
 void Samv71RtemsSerialInit(
 	void *private_data, const enum SystemBus bus_id,
 	const enum SystemDevice device_id,
 	const Serial_SamV71_Rtems_Conf_T *const device_configuration,
 	const Serial_SamV71_Rtems_Conf_T *const remote_device_configuration);
+
+/**
+ * @brief Function which implements receiving data from remote partition.
+ *
+ * Functions works in separate thread, which is initialized by
+ * SamV71SerialCcsdsInit
+ *
+ * @param private_data   Driver private data, allocated by runtime
+ */
 void Samv71RtemsSerialPoll(void *private_data);
+
+/**
+ * @brief Send data to remote partition.
+ *
+ * Function is used by runtime.
+ *
+ * @param private_data   Driver private data, allocated by runtime
+ * @param data           The Buffer which data to send to connected remote
+ * partition
+ * @param length         The size of the buffer
+ */
 void Samv71RtemsSerialSend(void *private_data, const uint8_t *const data,
 			   const size_t length);
 
--- src/samv71_rtems_serial/samv71_rtems_serial_internal.h
+++ src/samv71_rtems_serial/samv71_rtems_serial_internal.h
@@ -1,7 +1,23 @@
 #ifndef SAMV71_RTEMS_SERIAL_INTERNAL_H
 #define SAMV71_RTEMS_SERIAL_INTERNAL_H
 
+#include <Uart/Uart.h>
+
 #define DRIVER_TASK_STACK_SIZE 512
 #define DRIVER_TASK_PRIORITY 1
+
+/// \brief Uart configuration structure
+typedef struct {
+	Uart_Id id; /// Uart device identifier (0-4)
+	Uart_Parity parity; //< used parity bits
+	uint32_t baudrate; //< chosen baud rate
+} Hal_Uart_Config;
+
+/// \brief uart structure
+typedef struct {
+	Uart uart;
+	ByteFifo rxFifo;
+	ByteFifo txFifo;
+} Hal_Uart;
 
 #endif
