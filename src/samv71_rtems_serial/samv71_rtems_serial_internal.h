cd45fb9fa4da77365bfc6c237b0892fad86570c2
Rafał Babski <rbabski@n7space.com>
2025-10-16 12:13:48 +0200

    removed unused macros, added license header

--- src/samv71_rtems_serial/samv71_rtems_serial_internal.h
+++ src/samv71_rtems_serial/samv71_rtems_serial_internal.h
@@ -1,10 +1,31 @@
+/**@file
+ * This file is part of the TASTE SAMV71 RTEMS Drivers.
+ *
+ * @copyright 2025 N7 Space Sp. z o.o.
+ *
+ * Licensed under the ESA Public License (ESA-PL) Permissive (Type 3),
+ * Version 2.4 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://essr.esa.int/license/list
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef SAMV71_RTEMS_SERIAL_INTERNAL_H
 #define SAMV71_RTEMS_SERIAL_INTERNAL_H
 
-#include <Uart/Uart.h>
+/**
+ * @file     samv71_rtems_serial_internal.h
+ * @brief    Internal structures for driver SAMV71 UART
+ */
 
-#define DRIVER_TASK_STACK_SIZE 512
-#define DRIVER_TASK_PRIORITY 1
+#include <Uart/Uart.h>
 
 /// \brief Uart configuration structure
 typedef struct {
